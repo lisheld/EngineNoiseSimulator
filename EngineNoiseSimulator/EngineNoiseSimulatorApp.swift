0c875dc404a866f74e77796bd63b9d1d55e5e13d
lisheld <lsheldon@mit.edu>
2024-07-21 22:14:41 -0400

    Initial Commit

---
+++ EngineNoiseSimulator/EngineNoiseSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngineNoiseSimulatorApp.swift
+//  EngineNoiseSimulator
+//
+//  Created by Liam Sheldon on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct EngineNoiseSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
